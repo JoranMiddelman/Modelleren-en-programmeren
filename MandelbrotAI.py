45bb4391cdcfccb07f0e70a5f01031666d24e040
Zi-lon <118438321+Zi-lon@users.noreply.github.com>
2022-12-03 13:55:42 +0100

    Update MandelbrotAI.py
    pagina 1 klaar

--- MandelbrotAI.py
+++ MandelbrotAI.py
@@ -1,5 +1,6 @@
 #Zilon Huang & Joran Middelman
 
+#import benodigde libraries
 from tkinter import Frame, Label, Entry, Tk, Button
 from PIL.ImageDraw import Draw
 from PIL.ImageTk import PhotoImage 
@@ -22,27 +23,35 @@ InvoerMiddenY = Entry(scherm, width=10); InvoerMiddenY.place(x=100, y=40); Invoe
 
 #setup of schaal:
 tekst = Label(scherm, text="schaal:", font=("Arial", 18)); tekst.place(x=10, y=70);
-InvoerSchaal = Entry(scherm, width=10); InvoerSchaal.place(x=100, y=70); InvoerSchaal.insert(0,str(0.005));
+InvoerSchaal = Entry(scherm, width=10); InvoerSchaal.place(x=100, y=70); InvoerSchaal.insert(0,str(0.01));
 
 #setup of iterations:
 tekst = Label(scherm, text="iteraties:", font=("Arial", 18)); tekst.place(x=10, y=100);
-InvoerIteraties = Entry(scherm, width=10); InvoerIteraties.place(x=100, y=100); InvoerIteraties.insert(0,str(6));
+InvoerIteraties = Entry(scherm, width=10); InvoerIteraties.place(x=100, y=100); InvoerIteraties.insert(0,str(20));
 
 #setup van de knop
 knop = Button(scherm, text="Bereken", font=("Arial, 18"), height=5,width=10); knop.place(x=240,y=10);
 
 # Afbeelding van Mandelbrot
 afbeelding = Label(scherm); afbeelding.place(x=10,y=140)
+
 # Aantal pixels in de x en y richting
 width_x, height_y = 400, 400
 
+# toepassen van de formule en condities 
 def calc(x, y):
-    a = b = 0
-    for i in range(maxIt):
-        a, b = a*a - b*b + x, 2*a*b + y
-        if a*a + b*b > 4:
-            return i+1
-    return 0
+    a = 0; b = 0; mandelgetal = 0; pythagoras = 0
+    while pythagoras <= 2 and mandelgetal <= maxIt: #Afstand niet groter dan 2 en functie stopt als het bij de aangegeven max. iteraties komt. 
+        pythagoras = math.sqrt(a*a + b*b) # Berekend de afstand
+        a, b = a*a - b*b + x, 2*a*b + y # functie van de opgave
+        mandelgetal += 1 # Het mandelgetal van aantal keer dat f is toegepast
+        
+    if mandelgetal == 1: # Conditie als het mandel getal al 1 is dan geeft hij gelijk 1 terug
+        return 1
+    elif mandelgetal == maxIt: # Conditie als mandelgetal oneindig is dan stoppen we met het toepassen van de functie en zetten we het mandelgetal op max iteraites
+        return maxIt
+    else:
+        return mandelgetal # Als de vorige niet voordoen dan is het mandelgetal tussen 1 en maxIt
 
 def teken():
     plaatje = Image.new(mode="RGB", size=(width_x,height_y))
@@ -52,10 +61,18 @@ def teken():
         x = (row-200) * schaal + CoordinaatX
         for col in range(height_y):
             y = (col-200) * schaal + CoordinaatY
+            
             v = calc(x,y)
-            if v:
-              plaatje.putpixel((row, col), (255,255,0))
-              
+            if(v % 2 != 0):
+                plaatje.putpixel((row, col), (0 , 0, 0))
+            else:
+                plaatje.putpixel((row, col), (255 , 255, 255))
+            #else:
+            # hue = int(255 * v / maxIt)
+            # saturation = 255
+            # value = 255 if v < maxIt else 0
+            # plaatje.putpixel((row, col),(hue, saturation, value))  
+
     global omgezetPlaatje
     omgezetPlaatje = PhotoImage(plaatje)
     afbeelding.configure(image=omgezetPlaatje)
@@ -63,7 +80,7 @@ def teken():
 # Declaratie van globale variables
 CoordinaatX = 0; CoordinaatY = 0;schaal = 0; maxIt = 0
 
-def bereken():
+def bereken(): # functie vraagt de input van de gebruiker en pas ze toe in de functie teken()
     global CoordinaatX, CoordinaatY,schaal,maxIt
     try:
         CoordinaatX = float(InvoerMiddenX.get())
@@ -83,11 +100,3 @@ knop.configure(command=bereken)
 bereken()
 
 scherm.mainloop()
-
-# if(mandelnummer % 2 != 0):
-#                 plaatje.putpixel((row, col), (0 , 0, 0))
-#             else:
-#                 hue = int(255 * mandelnummer / maxIt)
-#                 saturation = 255
-#                 value = 255 if mandelnummer < maxIt else 0
-#             plaatje.putpixel((row, col),(hue, saturation, value))  
